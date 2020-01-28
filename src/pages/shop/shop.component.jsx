70adf0d90e38686f742fe0a37a821e5d4323cbbc
Victor Villavicencio <victor.villavicencio.10@gmail.com>
2020-01-28 14:47:00 -0500

    adding shop and collection items

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collection: SHOP_DATA
+    };
+  }
+  render() {
+    return (
+      <div className="shop-page">
+        {this.state.collection.map(({ id, ...collectionProps }) => (
+          <CollectionPreview key={id} {...collectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+export default ShopPage;
